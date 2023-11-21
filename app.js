fc62da1f4f1f64f62a501af1a84d74c2ad788d5a
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2023-11-21 21:10:03 +0000

    AutoPush

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById('btn');
-const color = document.querySelector(".color");
+const colors = ["green" , "red" , "rgba(133,122,200)" , "#f15025"]
+const btn = document.getElementById('btn')
+const color = document.querySelector(".color")
+ 
+btn.addEventListener('click',function (){
+    const randomnumber = randomcolor();
+    document.body.style.backgroundColor = colors[randomnumber];
+    color.textContent = colors[randomnumber]
 
-btn.addEventListener('click', function(){
-    // get random number between 0 - 3
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-});
+})
 
-function getRandomNumber(){
+
+function randomcolor(params) {
     return Math.floor(Math.random()*colors.length);
 }
 
